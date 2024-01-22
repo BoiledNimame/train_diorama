371fa6a9c3a8940f3dc14b9eecb447a28eaeba12
HadukiLaptop <haduki.kyouka@gmail.com>
2024-01-22 14:31:25 +0900

    java版でも開発

---
+++ java/com/trainJava/mortor/Pmw.java
@@ -0,0 +1,42 @@
+package com.trainJava.mortor;
+
+public class Pmw {
+    /**
+     * パルス入力による稼働率制御の為の稼働時間情報構築用メソッド
+     * 
+     * @param duration             {@code long } ms ミリ秒で指定する
+     * @param operation_persentage {@code int} %で指定
+     * @param max_divisions        {@code int} パルス波形の分割回数 下限は第4引数
+     *                             {@code min_WaitTime} に依存するため目安程度
+     * @param min_WaitTime         {@code int} ms ミリ秒で指定 目安は3~50ms程度
+     *                             そのマシンが正確に計測できる最短のミリ秒を指定する
+     * @return {runDuration[ms], sleepDuration[ms], repetitions[number of repeat]}
+     * @throws IllegalArgumentException 第二引数 {@code operation_persentage}
+     *                                  が101以上だと起こる
+     */
+    public static long[] pulse(long duration, int operation_persentage, int max_divisions, int min_WaitTime)
+            throws IllegalArgumentException {
+        if (100 < operation_persentage) {
+            throw new IllegalArgumentException("operation_persentageが100%を超えたため処理を停止します.");
+        }
+        long runDurationLong = Math.round(duration * ((double) operation_persentage / 100));
+        long slpDurationLong = duration - runDurationLong;
+        long divided_runDuration = Math.round((double) runDurationLong / max_divisions);
+        long divided_slpDuration = Math.round((double) slpDurationLong / max_divisions);
+        long repeat = max_divisions;
+        if (divided_runDuration < min_WaitTime || divided_slpDuration < min_WaitTime) {
+            // 正確に実行できず実際のdurationが間延びしてしまうので, 分割数を減らす
+            for (int i = max_divisions; i < 0; i--) {
+                runDurationLong = Math.round((double) duration * (operation_persentage / 100));
+                slpDurationLong = duration - runDurationLong;
+                divided_runDuration = Math.round((double) runDurationLong / i);
+                divided_slpDuration = Math.round((double) slpDurationLong / i);
+                if (min_WaitTime < divided_runDuration && min_WaitTime < divided_slpDuration) {
+                    repeat = i;
+                    break;
+                }
+            }
+        }
+        return new long[] { divided_runDuration, divided_slpDuration, repeat };
+    }
+}
--- python/pwn_test.py
+++ python/pwn_test.py
@@ -1,50 +1,36 @@
 import RPi.GPIO as GPIO
+import threading
 import time
+
+#####################################################################
+# 参照: https://qiita.com/montblanc18/items/05715730d99d450fd0d3
+# scheduler(number interval, method f, bool wait)
+# interval: sec
+# method: work
+# wait: can make next thread at running other thread
+#####################################################################
+
+def scheduler(interval, f, wait = True):
+    base_time = time.time()
+    next_time = 0
+    while True:
+        t = threading.Thread(target = f)
+        t.start()
+        if wait:
+            t.join()
+        next_time = ((base_time - time.time()) % interval) or interval
+        time.sleep(next_time)
+
 
 GPIO.setmode(GPIO.BCM)
 
-mortorDriverOutput:int = 5
-switchTrainWorkInput:int = 6
+mortor_driver_output_forword:int = 5
+mortor_driver_output_reverse:int = 6
+switch_train_work_input:int = 7
 
-GPIO.setup(mortorDriverOutput, GPIO.OUT)
-GPIO.setup(switchTrainWorkInput, GPIO.IN, pull_up_down=GPIO.PUD_HIGH)
+GPIO.setup(mortor_driver_output_forword, GPIO.OUT)
+GPIO.setup(switch_train_work_input, GPIO.IN, pull_up_down=GPIO.PUD_HIGH)
 
-# パルス出力によりモータの回転数を制御する
-# GPIONum = GPIOの番号
-# pulsetime = ミリ秒intで指定する
-# duration = ミリ秒intで指定する
-def PWM(GPIONum, pulsetime_millis, duration_millis):
-    if duration_millis > pulsetime_millis:
-        # pulsetime : seconds-> milliseconds
-        pulsetime_millis = pulsetime_millis/1000
-        duration_millis = duration_millis/1000
-        waitTime_millis:float = duration_millis-pulsetime_millis
-        for i in range(duration_millis):
-            GPIO.output(GPIONum, GPIO.HIGH)
-            time.sleep(pulsetime_millis)
-            GPIO.output(GPIONum, GPIO.LOW)
-            time.sleep(waitTime_millis)
-    else:
-        print("err: duration_is_too_short")
-
-while True:
-    if GPIO.input(switchTrainWorkInput):
-        # 駅にいるか判断
-        if GPIO.input(staion1_leadswitch|station2_leadswitch|station3_leadswitch):
-            # 何秒で加速し切るかを表す
-            kasokudo:int = 1000
-            # 少しずつ変えていく, その回数を決める
-            stepcount:int = 50
-
-            for i in range(kasokudo):
-                # パルスの速度を上げる間隔(ミリ秒): 0.5s
-                steptime_milliseconds:int = 500
-                # 稼働する時間(ミリ秒): 0.0000....
-                # これにiを反映させる.
-                pulse_milliseconds:int = i
-            PWM(mortorDriverOutput, pulse_milliseconds, steptime_milliseconds)
-    else:
-    
 
 
 # END
