0f75bc222f6184c4343877b4b9cc2f83731f4da4
HadukiDesktop <haduki.kyouka@gmail.com>
2024-01-22 17:33:17 +0900

    Mainは近々書き換えるのでtest.javaへ移動 & 追跡を入れとく

---
+++ train/src/main/java/com/traindiorama/Test.java
@@ -0,0 +1,23 @@
+package com.traindiorama;
+
+import com.traindiorama.mortor.Pmw;
+
+public class Test {
+    public static void main(String args[]) {
+        long duration = 10000; // 10s
+        int operation = 50; // 50%
+        int division = 300; // 20分割
+        int waittime = 30; // 30ms
+
+        System.out.println("----------------------------------------------------");
+        System.out.print("duration: " + duration + "ms");
+        System.out.println("    operation: " + operation + "%");
+        System.out.print("division: *" + division);
+        System.out.println("        waittime: " + waittime + "ms");
+        long[] result = Pmw.pulse(duration, operation, division, waittime);
+        System.out.println("result: run, slp, div: " + result[0] + "ms, " + result[1] + "ms, *" + result[2]);
+        System.out.println("result_duration: " + ((result[0]+result[1])*result[2]) + "ms");
+        System.out.println("err:" + (((result[0]+result[1])*result[2])-duration) + "ms");
+        System.out.println("----------------------------------------------------");
+    }    
+}
