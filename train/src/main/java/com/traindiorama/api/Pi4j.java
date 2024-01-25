029e4417a00d87edfe2034a61ca9eace0e1ab0a6
HadukiLaptop <haduki.kyouka@gmail.com>
2024-01-25 12:51:47 +0900

    公式のjavadocぐらい真面目に書けや

--- train/src/main/java/com/traindiorama/api/Pi4j.java
+++ train/src/main/java/com/traindiorama/api/Pi4j.java
@@ -10,7 +10,10 @@ import com.pi4j.platform.Platforms;
 import com.pi4j.util.Console;
 
 public class Pi4j {
+    // ref-> 読み物, doc-> 専門的
     // ref: https://pi4j.com/getting-started/minimal-example-application/
+    // doc:
+    // https://javadoc.io/doc/com.pi4j/pi4j-core/latest/com.pi4j/module-summary.html
     public static Context setupContext() {
         return Pi4J.newAutoContext();
     }
@@ -31,14 +34,14 @@ public class Pi4j {
     /**
      * ref: https://pi4j.com/documentation/io-examples/digital-input/
      * 
-     * @param context      セットアップされたContextオブジェクトを渡してください.
-     * @param id           このデジタル入力オブジェクトを表す一意の文字列です.
-     * @param name         {@code toString()}や{@code describe()}で使用されます.
-     * @param pinBCMNumber BCM: GPIOの数字で指定する, BCM方式で表されるピンの番号です.
+     * @param context      セットアップされたContextオブジェクト.
+     * @param id           このデジタル入力オブジェクトを表す一意の文字列.
+     * @param name         {@code toString()}や{@code describe()}で使用される.
+     * @param pinBCMNumber BCM: GPIOの数字で指定する, BCM方式で表されるピンの番号.
      * @param pr           入力抵抗を表します. UPは回路が繋がるとGPIOはHIGH(3.3V), 切断されるとLOW(0V),
-     *                     DOWNはその逆を表します.
-     * @param debounce     デバウンス(連続入力を防ぐ)時間を指定します. ミリ秒です.
-     * @param provider     ref:https://pi4j.com/documentation/providers/
+     *                     DOWNはその逆を表す.
+     * @param debounce     デバウンス(連続入力を防ぐ)時間を指定(ミリ秒).
+     * @param provider     {@see https://pi4j.com/documentation/providers/}
      *                     推奨:pigpio .
      * @return
      */
@@ -49,14 +52,16 @@ public class Pi4j {
     }
 
     /**
+     * ref: https://pi4j.com/documentation/io-examples/digital-output/
      * 
-     * @param context
-     * @param id
-     * @param name
-     * @param pinBCMNumber
-     * @param shutdown
-     * @param initial
-     * @param provider
+     * @param context      セットアップされたContextオブジェクト.
+     * @param id           このデジタル入力オブジェクトを表す一意の文字列.
+     * @param name         {@code toString()}や{@code describe()}で使用される文字列.
+     * @param pinBCMNumber BCM: GPIOの数字で指定する, BCM方式で表されるピンの番号.
+     * @param shutdown     シャットダウン時の電圧を指定.
+     * @param initial      初期状態での電圧を指定.
+     * @param provider     {@see https://pi4j.com/documentation/providers/}
+     *                     推奨:pigpio .
      * @return 作成済みのセットアップされたデジタル出力オブジェクトを返します.
      */
     public static DigitalOutput setupGPIO(Context context, String id, String name, int pinBCMNumber,
