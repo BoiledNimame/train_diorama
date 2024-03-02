3d63fb366e79b1b49e6e23f30417a5cef3ead7ff
HadukiLaptop <haduki.kyouka@gmail.com>
2024-03-02 21:41:33 +0900

    時間切れ, commitしておく

--- train/src/main/java/com/traindiorama/mortor/SteppingAcceleration.java
+++ train/src/main/java/com/traindiorama/mortor/SteppingAcceleration.java
@@ -1,5 +1,22 @@
 package com.traindiorama.mortor;
 
+import java.util.Arrays;
+
 public class SteppingAcceleration {
-    
+    public static int[] acccel(int initialOperationPersentage, int lastOperationPersentage, long duration, int division) {
+        int[] result = new int[]{};
+        /*
+         * duration = 全体
+         * division = 分割の精度
+         * initPsec = 初速
+         * lastPsec = 終端
+         */
+        while (true) {
+            
+            if (duration < Arrays.stream(result).sum()) {
+                break;
+            }
+        }
+        return new int[]{};
+    }
 }
