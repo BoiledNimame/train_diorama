36037bf195efd9539b8f07540d8c975171105baa
HadukiLaptop <haduki.kyouka@gmail.com>
2024-03-09 19:43:32 +0900

    270のほうらしい

--- train/src/main/java/com/traindiorama/pulse/MortorData.java
+++ train/src/main/java/com/traindiorama/pulse/MortorData.java
@@ -8,7 +8,7 @@ public interface MortorData {
 
     public long NeutralPeriodMicroseconds = 2000;
 
-    public short Angle_Degree_MaxRangePulse = 180;
-    public short Angle_Degree_NeutralPulse = 90;
+    public short Angle_Degree_MaxRangePulse = 270;
+    public short Angle_Degree_NeutralPulse = 135;
     public short Angle_Degree_MinRangePulse = 0;
 }
