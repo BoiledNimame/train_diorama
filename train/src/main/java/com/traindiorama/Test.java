1759c3100bb4dba4a7a8e763554ce63352882c46
HadukiDesktop <haduki.kyouka@gmail.com>
2024-03-10 23:46:59 +0900

    忘れそうなことメモ

--- train/src/main/java/com/traindiorama/Test.java
+++ train/src/main/java/com/traindiorama/Test.java
@@ -8,5 +8,11 @@ public class Test implements MortorData {
         int angle = 90;
         int result = PWM.buildMortorPulse(angle, MaxPulseRangeMicroseconds);
         System.out.println("angle: " + angle + "°, result: " + result + "μs");
+
+        // pwmというのは角度を保たせる技術
+        // よって, 外力による角度変更を防ぐために現在位置を指定するパルスを常に送らなければならない
+        // そのため, 別スレを走らせてそれで常にGPIOをON-OFFするパルスを送らせなければならない
+
+        // パルス幅 -> @see com.traindiorama.pulse.MortorData
     }
 }
