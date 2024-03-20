85d9704d79cba3b7efedc70d30ebdde6abe3ca29
artglass-led <example@email>
2024-03-20 21:13:43 +0900

    動くのは動くの分かったんで一旦raspberrypiは切る(ホットカーペットの電源かえして)

--- train/src/main/java/com/traindiorama/Main.java
+++ train/src/main/java/com/traindiorama/Main.java
@@ -1,4 +1,28 @@
 package com.traindiorama;
 
 public class Main {
+    private static Main instance;
+    public final boolean isDebug;
+
+    // インスタンス化(singleton)
+
+    private Main(String[] args) {
+        isDebug = args.length != 0 ? args[0].equals("-debug") : false;
+    }
+
+    // main
+
+    public static void main(String[] args) {
+        instance = new Main(args);
+    }
+
+    // 後から要りそうになるメソッド
+
+    public static Main getInstance() {
+        return instance;
+    }
+
+    public static boolean isDebug() {
+        return Main.getInstance().isDebug;
+    }
 }
