a81933d79b289271e1b49b049d245b5adeeb2c5a
HadukiDesktop <haduki.kyouka@gmail.com>
2024-03-22 21:54:53 +0900

    pwmで最低限いるやつを実装して値の読み出しまで完了

--- train/src/main/java/com/traindiorama/gui/WindowController.java
+++ train/src/main/java/com/traindiorama/gui/WindowController.java
@@ -1,5 +1,44 @@
 package com.traindiorama.gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Slider;
+
 public class WindowController {
-    
+    private static boolean allwaysApplyDuty = false;
+    private static double duty = 0D;
+
+    //------------------------------------// FXML //------------------------------------//
+
+    @FXML Slider mortorSliderDuty;
+    @FXML Button mortorApplyButton;
+    @FXML CheckBox mortorCheckboxAllwaysApply;
+
+    @FXML
+        public void onMouse() {
+            if (allwaysApplyDuty) {
+                duty = mortorSliderDuty.getValue();
+            }
+        }
+
+    @FXML
+        public void onCheckBoxClicked() {
+            allwaysApplyDuty = mortorCheckboxAllwaysApply.selectedProperty().get();
+        }
+
+    @FXML
+        public void onClicked() {
+            duty = mortorSliderDuty.getValue();
+        }
+
+    //------------------------------------// FXML //------------------------------------//
+
+    public static boolean allwaysApplyDuty() {
+        return allwaysApplyDuty;
+    }
+
+    public static int getDutyAsInt() {
+        return Math.toIntExact(Math.round(duty));
+    }
 }
